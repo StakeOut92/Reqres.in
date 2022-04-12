2083ee40cb282961e88d7d57ad6970fac522881a
StakeOut92 <inozemtcew@gmail.com>
2022-04-12 18:37:21 +0300

    Add Cucumber test and features

--- src/test/java/runners/BookingComTest.java
+++ src/test/java/runners/BookingComTest.java
@@ -4,13 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-features = {"src/test/resources/features"}
-
-
-
-
+        features = {"src/test/resources/features"},
+        glue = {"steps"},
+        plugin = {"pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json"}
 )
 public class BookingComTest extends AbstractTestNGCucumberTests {
-
-
 }
